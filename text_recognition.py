68ad6a0a1dfcd33763f73353f1e615f2d2965ca0
Raveena29-ai <sarikondaraveena@gmail.com>
2026-07-03 21:27:20 +0530

    Added Project 4 Python code

---
+++ text_recognition.py
@@ -0,0 +1,9 @@
+sample_text = """
+Artificial Intelligence
+Project 4
+Image or Text Recognition (Basic)
+"""
+
+print("===== Recognized Text =====")
+print(sample_text)
+print("===========================")
